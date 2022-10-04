620dd3dbcf8ca30f8d75ddf7f1e8e88b84aa3050
harsh95jadhav <jadhavharsh99@gmail.com>
2022-10-04 20:53:04 +0530

    first program execution in js
    first program execution in js

---
+++ assignA.js
@@ -0,0 +1,7 @@
+
+console.log("Harshvardhan Dattatray Jadhav");
+console.log("My hobbies-reading,cooking,playing games");
+console.info("Personal strength-Hard working,quick learner,honest");
+console.warn("Weakness-i cant say no when someone asking for help,get easily emotional");
+console.log(30+50);
+console.log(49*479);
