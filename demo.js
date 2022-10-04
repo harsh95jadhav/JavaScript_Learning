2f3a5cff1bccf74ff61e6f02d5b6eee25b866088
harsh95jadhav <jadhavharsh99@gmail.com>
2022-10-04 09:10:14 +0530

    basics of js
    basics of js

---
+++ demo.js
@@ -0,0 +1,9 @@
+// This is my first PerformanceNavigationTiming
+// Now i know how to run it
+console.log("hello");
+console.log(" my name is Harshvardhan ");
+// info stands for information
+console.info("i am a engineer ");
+// worn stand for worning
+console.warn("it is a danger zone ");
+console.error("some errors are here");
