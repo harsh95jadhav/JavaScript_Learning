0915333366310e661a5e8ceb5d69b36e9b5c99c3
harsh95jadhav <jadhavharsh99@gmail.com>
2022-10-08 08:18:37 +0530

    assignment 02 03
    assignment 02 03

---
+++ 02_assignment.js
@@ -0,0 +1,7 @@
+var birthYear = "1995";
+var firstName = "harsh";
+var isMarried = "true";
+
+console.log(birthYear, typeof 1995);
+console.log(firstName, typeof "harsh");
+console.log(isMarried, typeof true);
---
+++ 03_assignment.js
@@ -0,0 +1,17 @@
+console.log("=======without argument=======");
+function first_name(){
+    console.log("harshvardhan jadhav");
+    console.log("i have completed BE in mechanical engineering");
+
+}
+first_name()
+console.log("============with argument or return===================");
+
+function functionName(firstname,lastname){
+    console.log(functionName);
+    return firstname + lastname
+
+}
+var value = "harsh,jadhav"
+functionName(firstname,lastname)
+console.log(value);
